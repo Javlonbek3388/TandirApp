a27bdf8fc387fd90c39a7d6b12b9d3bf62da8191
Javlonbek Boriyev <153982223+Javlonbek3388@users.noreply.github.com>
2024-10-06 11:49:00 +0500

    Add main serializers

---
+++ main/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from .models import Language, Quiz, Question, Participant, Answer, Result
+
+
+class LanguageViewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Language
+        fields = '__all__'
+
+
+class QuizViewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Quiz
+        fields = '__all__'
+
+
+class QuestionViewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = '__all__'
+
+
+class ParticipantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Participant
+        fields = '__all__'
+
+
+class AnswerViewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Answer
+        fields = '__all__'
+
+
+class ResultView(serializers.ModelSerializer):
+    class Meta:
+        model = Result
+        fields = '__all__'
