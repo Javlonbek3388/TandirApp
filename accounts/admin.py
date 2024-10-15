8d0c792a38197d0ed51c8bd369e97a4d4f4d20ea
Javlonbek Boriyev <153982223+Javlonbek3388@users.noreply.github.com>
2024-10-15 22:41:43 +0500

    Update admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 from .models import *
 
 
-class UserAdmin(admin.ModelAdmin):
-    list_display = ['full_name', 'username', 'programming_language']
-    ordering = ['created_at']
-
+admin.site.register(User)
 
 admin.site.register(Profile)
--- main/admin.py
+++ main/admin.py
@@ -5,5 +5,5 @@ from .models import *
 admin.site.register(Language)
 admin.site.register(Quiz)
 admin.site.register(Question)
-
+admin.site.register(Participant)
 admin.site.register(Result)
