f3ef66d462214042337908960e080d296e3f067b
Javlonbek Boriyev <153982223+Javlonbek3388@users.noreply.github.com>
2024-10-15 23:30:11 +0500

    Update permissions

--- main/views.py
+++ main/views.py
@@ -3,21 +3,43 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Language, Quiz, Question, Participant, Answer, Result
 from .serializers import (LanguageViewSerializer, QuizViewSerializer, QuestionViewSerializer,
                           ParticipantSerializer, AnswerViewSerializer, ResultViewSerializer)
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
 
 
 class LanguageView(ModelViewSet):
     queryset = Language.objects.all()
     serializer_class = LanguageViewSerializer
 
+    def get_permissions(self):
+        if self.request.method in ['POST', 'PUT', 'DELETE']:
+            self.permission_classes = [IsAdminUser]
+        else:
+            self.permission_classes = [AllowAny]
+        return super(LanguageView, self).get_permissions()
+
 
 class QuizView(ModelViewSet):
     queryset = Quiz.objects.all()
     serializer_class = QuizViewSerializer
 
+    def get_permissions(self):
+        if self.request.method in ['PUT', 'POST', 'DELETE']:
+            self.permission_classes = [IsAdminUser]
+        else:
+            self.permission_classes = [AllowAny]
+        return super(QuizView, self).get_permissions()
+
 
 class QuestionView(ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionViewSerializer
+
+    def get_permissions(self):
+        if self.request.method in ['PUT', 'POST', 'DELETE']:
+            self.permission_classes = [IsAdminUser]
+        else:
+            self.permission_classes = [AllowAny]
+        return super(QuestionView, self).get_permissions()
 
 
 class ParticipantView(ModelViewSet):
@@ -33,4 +55,11 @@ class AnswerView(ModelViewSet):
 class ResultView(ModelViewSet):
     queryset = Result.objects.all()
     serializer_class = ResultViewSerializer
+
+    def get_permissions(self):
+        if self.request.method in ['PUT', 'POST', 'DELETE']:
+            self.permission_classes = [IsAdminUser]
+        else:
+            self.permission_classes = [AllowAny]
+        return super(ResultView, self).get_permissions()
 
